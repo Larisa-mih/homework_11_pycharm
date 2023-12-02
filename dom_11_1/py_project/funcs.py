323ba8984baa88075ce2f888dba0180b44715706
Larisa-mih <larisa.mih2004@gmail.com>
2023-12-02 23:06:24 +0300

    modify function

--- dom_11_1/py_project/funcs.py
+++ dom_11_1/py_project/funcs.py
@@ -1,4 +1,14 @@
-def plus(a, b):
-    return a + b
-print(plus(2, 2))
+def add_numbers(x, y):
+    summ = x + y
+    return summ
 
+
+num1 = 15
+num2 = 20
+
+print("The sum is", add_numbers(num1, num2))
+
+a = 10
+b = 5
+c = a - b
+print(c)
