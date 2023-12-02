f541aefbc75753405f6490fa6945650da68e4963
Larisa-mih <larisa.mih2004@gmail.com>
2023-12-03 00:34:44 +0300

    modify change second function

--- dom_11_1/main.py
+++ dom_11_1/main.py
@@ -1,8 +1,9 @@
-from dom_11_1.py_project.funcs import add_numbers
+from dom_11_1.py_project.funcs import add_numbers, subtraction
 
-num1 = 15
-num2 = 20
+num1 = 20
+num2 = 15
 
 
 if __name__ == '__main__':
     print("The sum is", add_numbers(num1, num2))
+    print(num1 - num2)
